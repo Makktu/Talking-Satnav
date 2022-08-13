bd82fc42071cbf372033a2e595822774d7edf6b9
John McNamara <johnmcna@gmail.com>
2022-08-13 21:14:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const testBtn = document.getElementById('test-btn');
 
 testBtn.addEventListener('click', () => {
     speakingComputer(
-        `John Mack Vay, is a fucking cunt. Hello ${userName}. Please state your destination. You can tell me to "start again" whenever you like.`
+        `Hello ${userName}. Please state your destination. You can tell me to "start again" whenever you like.`
     );
     // speakingComputer(
     //     `Please state your destination. You can say - "reset" - at any time, to start over.`
